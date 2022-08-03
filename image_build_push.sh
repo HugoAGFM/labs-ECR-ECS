4be10778acc3c8e2095090c3ce614d5ee62fbf02
Hugo Mendonça <96540348+HugoAGFM@users.noreply.github.com>
2022-08-03 16:52:51 -0300

    changing docker image to apache, excluding python file

--- image_build_push.sh
+++ image_build_push.sh
@@ -3,7 +3,6 @@ AWS_ACCOUNT_ID=$(jq -r .outputs.aws_account_id.value ./terraform/terraform.tfsta
 ECR_NAME=$(jq -r .outputs.ecr_name.value ./terraform/terraform.tfstate)
 ECR_URI=$(jq -r .outputs.ecr_uri.value ./terraform/terraform.tfstate)
 
-
 aws ecr get-login-password --region $REGION | docker login -u AWS --password-stdin $AWS_ACCOUNT_ID".dkr.ecr."$REGION".amazonaws.com"
 
 docker build -t $ECR_NAME ./docker
