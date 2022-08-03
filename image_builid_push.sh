5923a4c48447d38c453c5fade87fb16e33e6521f
Hugo Mendonça <96540348+HugoAGFM@users.noreply.github.com>
2022-08-03 11:46:31 -0300

    .sh file renamed

--- image_builid_push.sh
+++
@@ -1,7 +0,0 @@
-aws ecr get-login-password --region $REGION | docker login -u AWS --password-stdin $AWS_ACCOUNT_ID".dkr.ecr."$REGION".amazonaws.com"
-
-docker build -t $ECR_NAME ./docker
-
-docker tag $ECR_NAME":latest" $ECR_URI":latest"
-
-docker push $ECR_URI":latest"
