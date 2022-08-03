89d41716f8ecb0fce0df3f39fc2ff7c042518871
Hugo Mendonça <96540348+HugoAGFM@users.noreply.github.com>
2022-08-03 09:06:50 -0300

    adding ECR with terraform and docker file

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.24"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "ecr" {
+  name         = "container-images-repo"
+  force_delete = true
+
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
---
+++ terraform/output.tf
@@ -0,0 +1,3 @@
+output "ECR-URI" {
+  value = aws_ecr_repository.ecr.repository_url
+}
